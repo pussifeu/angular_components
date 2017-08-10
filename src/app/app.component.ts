868caa818f7a692f957e08f85c1e75de9e6aa5df
Antonio Rakotoarivony <antonio.rakotarivony@adictiz.com>
2017-08-10 14:53:30 +0200

    use less, add event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app';
+  value: string;
+  constructor() {
+    this.value = '';
+  }
+  changeValue (event) {
+    console.log('changeValue', event);
+  }
+
+  saveValue (event) {
+    this.value = event;
+    console.log('saveValue', event);
+  }
 }
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.less']
 })
 export class ButtonComponent implements OnInit {
-
-  constructor() { }
+  value: any;
+  @Output() onChangeBtnValue: EventEmitter<string>;
+  @Output() onSaveBtnValue: EventEmitter<string>;
+  constructor() {
+    this.value = '';
+    this.onChangeBtnValue = new EventEmitter<string>();
+    this.onSaveBtnValue = new EventEmitter<string>();
+  }
 
   ngOnInit() {
+  }
+
+  selectNumber (event, value: number) {
+    if (typeof (value) === 'number') {
+      if (this.value.length < 4) {
+        this.value = this.value.toString() + value.toString();
+        this.onChangeBtnValue.emit(this.value);
+      }
+    }
+  }
+
+  selectAction (event, action: string) {
+    if (action === 'cancel') {
+      this.value = this.value.slice(0, this.value.length - 1);
+      this.onChangeBtnValue.emit(this.value);
+    }
+    if (action === 'validate') {
+      this.onSaveBtnValue.emit(this.value);
+    }
   }
 
 }
